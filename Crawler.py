da1502c71fa67706a4f467b52b2df9b00a60d3eb
y2k2016 <471785083@qq.com>
2016-09-26 15:15:11 +0800

    bug and bug

--- Crawler.py
+++ Crawler.py
@@ -11,10 +11,10 @@ class Crawler(object):
 
 		print()
 		print("In crawler:")
-		print("paraDict: " + str(self.paraDict))
-		print("resultQueue: " + str(self.resultQueue))
-		print("flagQueue: " + str(self.flagQueue))
-		print("pauseEvent: " + str(self.pauseEvent))
+		print("paraDict: " + str(self.paraDict))			#see ParaKeys.py
+		print("resultQueue: " + str(self.resultQueue))		#put the filename or problem url in the queue
+		print("flagQueue: " + str(self.flagQueue))			#when finish the task put a string "STOP" in it
+		print("pauseEvent: " + str(self.pauseEvent))		#use to pause the program (the usage: self.pauseEvent.wait())
 		print()
 
 
@@ -22,6 +22,7 @@ class Crawler(object):
 		import time
 		for i in range(10):
 			time.sleep(1)
+			self.pauseEvent.wait()
 			self.resultQueue.put("filename" + str(i))
 		self.flagQueue.put("STOP")
 
--- ParaKeys.py
+++ ParaKeys.py
@@ -2,12 +2,9 @@
 
 class ParaKeys:
 	
-	ISFILESEARCH = "ISFILESEARCH"
-	FILETYPESLIST = "FILETYPESLIST"
-	LOGININFOS = "LOGININFOS"
-	FILESTARTURL = "FILESTARTURL"
-	SQLSTARTURL = "SQLSTARTURL"
-	KEYWORDS = "KEYWORDS"
-	RESULTQUEUE = "RESULTQUEUE"
-	FLAGQUEUE = "FLAGQUEUE"
-	PAUSEEVENT = "PAUSEEVENT"
+	ISFILESEARCH = "ISFILESEARCH"	#boolean：search file for true; sql test for false
+	FILETYPESLIST = "FILETYPESLIST"	#list: including file types
+	LOGININFOS = "LOGININFOS"		#[(account1,password1),(acoount2,password2),...]
+	FILESTARTURL = "FILESTARTURL"	#root url for file search
+	SQLSTARTURL = "SQLSTARTURL"		#root url for sql test
+	KEYWORDS = "KEYWORDS"			#filter words
