efb242b1ae124a7b7bdc571e2445cf319aacfd5f
zyssky <2462084760@qq.com>
2016-09-29 20:24:05 +0800

    使用search打开网页，只支持get方式，parser获取链接
    链接类型还可自行添加规则或种类，main中带有例子，chardet为解码用的，从requests弄过来的

---
+++ Mparser.py
@@ -0,0 +1,261 @@
+from html import parser
+
+import re
+
+class Myparser(parser.HTMLParser):
+    def __init__(self,url):
+        parser.HTMLParser.__init__(self)
+        self.links = []
+        self.scripts=[]
+        self.imgs=[]
+        self.others=[]
+        self.pdfs=[]
+        self.docs=[]
+        self.ppts=[]
+        self.xlss=[]
+        self.mp3s=[]
+        self.mp4s=[]
+
+        self.need2feed=True
+
+        self.url=url
+        self.node=url.split('/')
+        self.node.pop(1)
+        
+        if len(self.node) > 2:
+            self.node=self.node[:-1]
+
+        self.same_domain=False
+
+        self.ignore_type=["text/css"]
+
+    def check_response(self,response):
+        if response:
+            content_type=response.getheader('Content-Type')
+            if content_type and 'html' not in content_type:
+                self.need2feed=False
+                self.classify(content_type,response.geturl())
+
+
+    def set_same_domain(self,flag=True):
+        self.same_domain=flag
+
+    def format_uri(self,uri):
+        if uri.startswith('http'):
+            if self.same_domain and self.node[1] not in uri:
+                return
+            else:
+                return uri
+        if uri.startswith('{'):
+            return
+        if uri.startswith('/'):
+            return self.node[0]+'//'+self.node[1]+uri
+        if uri.startswith('../../'):
+            value=self.node[0]+'//'
+            for i in self.node[1:-2]:
+                value+=i+'/'
+            value+=uri.replace('../../','')
+            return value
+        if uri.startswith('../'):
+            value=self.node[0]+'//'
+            for i in self.node[1:-1]:
+                value+=i+'/'
+            value+=uri.replace('../','')
+            return value
+        if uri=="":
+            return self.url
+        current_uri=self.node[0]+'//'
+        for i in self.node[1:]:
+            current_uri+=i+'/'
+        return current_uri+uri
+
+
+    def handle_starttag(self, tag, attrs):
+        #print "Encountered the beginning of a %s tag" % tag
+        # print('init')
+        if tag == "img":
+            if len(attrs) == 0: pass
+            else:
+                # print(attrs)
+                for (variable,value) in attrs:
+                    if variable == "src":
+                        self.imgs.append(self.format_uri(value))
+            return
+
+
+        if tag == "script":
+            if len(attrs) == 0: pass
+            else:
+                for (variable,value) in attrs:
+                    if variable == "src":
+                        self.scripts.append(self.format_uri(value))
+            return
+
+        if len(attrs) == 0: pass
+        else:
+            content_type=None
+            link=None
+            for (variable,value) in attrs:
+                if variable == 'type' :
+                    content_type=value
+                if variable == 'src':
+                    link=self.format_uri(value)
+                if variable == 'href':
+                    link=self.format_uri(value)
+            self.classify(content_type,link)
+            # print(attrs)
+
+
+    def classify(self,content_type,value):
+        # print(content_type)
+        if not value:
+            return
+
+        if not content_type or content_type == 'text/html':
+            self.links.append(self.format_uri(value))
+            return
+
+        if content_type in self.ignore_type:
+            return
+            
+        if content_type == 'application/pdf':
+            self.pdfs.append(self.format_uri(value))
+            return
+
+        if content_type == 'application/x-jpg' or content_type ==  'image/jpeg' or content_type ==  'application/x-jpe':
+            print(content_type)
+            self.imgs.append(self.format_uri(value))
+            return
+
+
+        if content_type == 'application/ms-word' or content_type == 'application/x-word':
+            self.docs.append(self.format_uri(value))
+            return
+
+        if content_type == 'application/x-ppt' or content_type ==  'application/vnd.ms-powerpoint':
+            self.ppts.append(self.format_uri(value))
+            return
+
+
+        if content_type == 'application/vnd.ms-excel' or content_type ==  'application/x-xls':
+            self.xlss.append(self.format_uri(value))
+            return
+
+
+        if content_type == 'audio/mp3':
+            self.mp3s.append(self.format_uri(value))
+            return
+
+        if content_type == 'video/mpeg4':
+            self.mp4s.append(self.format_uri(value))
+            return
+
+
+        if content_type == 'application/vnd.rn-realmedia-vbr':
+            self.mp4s.append(self.format_uri(value))
+            return
+
+
+        if content_type == 'video/avi':
+            self.mp4s.append(self.format_uri(value))
+            return
+
+        else:
+            self.others.append(self.format_uri(value))
+
+    @property
+    def link_list(self):
+        temp=set(self.links)
+        if None in temp:
+            temp.remove(None)
+        return list(temp)
+
+    @property
+    def img_list(self):
+        temp=set(self.imgs)
+        if None in temp:
+            temp.remove(None)
+        return list(temp)
+
+    @property
+    def script_list(self):
+        temp=set(self.scripts)
+        if None in temp:
+            temp.remove(None)
+        return list(temp)
+
+
+    @property
+    def other_list(self):
+        temp=set(self.others)
+        if None in temp:
+            temp.remove(None)
+        return list(temp)
+
+
+    @property
+    def pdf_list(self):
+        try:
+            temp=set(self.pdfs)
+            if None in temp:
+                temp.remove(None)
+            return list(temp)
+        except:
+            return []
+
+
+    @property
+    def doc_list(self):
+        try:
+            temp=set(self.docs)
+            if None in temp:
+                temp.remove(None)
+            return list(temp)
+        except:
+            return []
+
+    @property
+    def ppt_list(self):
+        try:
+            temp=set(self.ppts)
+            if None in temp:
+                temp.remove(None)
+            return list(temp)
+        except:
+            return []
+
+
+    @property
+    def xls_list(self):
+        try:
+            temp=set(self.xlss)
+            if None in temp:
+                temp.remove(None)
+            return list(temp)
+        except:
+            return []
+
+    @property
+    def mp3_list(self):
+        try:
+            temp=set(self.mp3s)
+            if None in temp:
+                temp.remove(None)
+            return list(temp)
+        except:
+            return []
+
+    @property
+    def mp4_list(self):
+        try:
+            temp=set(self.mp4s)
+            if None in temp:
+                temp.remove(None)
+            return list(temp)
+        except:
+            return []
+
+    #to be add func like above
+
+
+    
---
+++ Search.py
@@ -0,0 +1,141 @@
+from urllib import request,parse
+import http.cookiejar
+import re
+import chardet
+import zlib
+from Mparser import Myparser
+import os
+
+#if the request headers add 'Accept-Encoding': 'gzip,deflate' ,then this func is need
+def decodeGZip(rawPageData):
+    if rawPageData.info().get(u"Content-Encoding") == "gzip":
+        try:
+            page_content = zlib.decompress(rawPageData.read(), 16 + zlib.MAX_WBITS)
+            print('已解压')
+        except zlib.error as ziperror:
+            print('解压出错')
+            print('出错解压页面:' + rawPageData.geturl())
+            print ('错误信息：')
+            print (zlib.error)
+            return ''
+    else:
+        page_content = rawPageData.read()
+    return page_content
+
+def decode(content,encoding):
+	if isinstance(content,bytes):
+		try:
+			content = str( content,encoding=encoding,errors='replace')
+		except:
+			content = str( content,errors='replace')
+	return content
+
+def apparent_encoding(content):
+	if not isinstance(content,bytes):
+		return 
+	try:
+		encoding=chardet.detect(content)['encoding']
+	except:
+		return 'utf-8'
+	if encoding:
+		return encoding
+	else:
+		return 'utf-8'
+
+def retrieve(url,filename,path):
+	try:
+		os.mkdir
+		r=request.urlopen(url)
+		f=r.read()
+		File = open( path+filename,"wb" )
+		File.write(f)
+		File.close()
+	except Exception as e:
+		print(e)
+
+
+
+class search():
+	"""docstring for spider"""
+	def __init__(self):
+		self.url = ''
+
+		self.content=''
+
+		self.response=None
+
+		self.cookie=http.cookiejar.LWPCookieJar()
+		self.cookie_support=request.HTTPCookieProcessor(self.cookie)
+		self.opener=request.build_opener(self.cookie_support,request.HTTPHandler)
+		request.install_opener(self.opener)
+
+		self.headers={'User-Agent':"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+		    		 'Connection': 'keep-alive','Content-Type': 'application/x-www-form-urlencoded'}
+
+
+
+	def get(self,url,timeout=8):
+
+		self.headers['Referer']=url               
+		self.headers['Origin']=url
+		self.url=url
+
+		req=request.Request(url,headers=self.headers)       
+
+		try:
+			self.response=self.opener.open(req,timeout=timeout)      
+
+			# help(response)
+
+			self.url=self.response.geturl()
+
+			
+
+		except Exception as e:
+			print(e)
+		else:
+			pass
+		finally:
+			pass
+
+	@property
+	def text(self):
+		if self.response:
+			self.content = decodeGZip(self.response)       #解压或decode，得到str形式的html
+			content_type=self.response.getheader('Content-Type')
+			pattern=re.compile(r'.+?charset=(.+?)$')
+			# print((content_type))
+			try:
+				match=pattern.match(content_type)
+				encoding=match.group(1)
+				# print(encoding)
+			except:
+				encoding=apparent_encoding(self.content)
+			
+			return decode(self.content,encoding)
+		else:
+			return ""
+	
+
+
+if __name__ == '__main__':
+	#first of all , the url form must be correct
+	s=search()
+	s.get('http://jcta.alljournals.ac.cn/cta_cn/ch/reader/create_pdf.aspx?file_no=CCTA160019&flag=1&journal_id=cta_cn&year_id=2016')
+	p=Myparser(s.url)
+	p.check_response(s.response)
+
+	#set the rule to fliter the url, default is False
+	p.set_same_domain(True)
+
+	if p.need2feed:
+		p.feed(s.text)
+
+	print(p.link_list)
+	print(p.img_list)
+	print(p.pdf_list)
+	print(p.doc_list)
+	print(p.other_list)
+
+	for i in p.pdf_list:
+		retrieve(i,'pdf2.pdf','')
